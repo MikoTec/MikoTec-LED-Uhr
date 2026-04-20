8ba2bb411f085c9ed159e05bb2db2d037cfefdd1
MikoTec <mikotec82@gmail.com>
2026-04-20 16:01:17 +0000

    v1.5: Zeitstempel mit globalem Cache (keine TimeLib-Aufrufe in logTS), SSDP-Spam entfernt - version.json bleibt auf v1.3 bis Test erfolgreich

--- MikoTec-LED-Uhr.ino
+++ MikoTec-LED-Uhr.ino
@@ -121,25 +121,34 @@ class DualPrint : public Print {
 
 DualPrint dualOut;
 
-// Zeitstempel direkt in Serial und Ringpuffer schreiben
-// Wird NICHT ueber DualPrint geleitet - kein Crash-Risiko
-void logTS() {
-  if (year() > 2000) {
-    char ts[23];
+// Globaler Zeitstempel-Cache - wird nur einmal pro Sekunde aktualisiert
+char tsCache[23] = "";
+int tsLastSecond = -1;
+
+void updateTimestampCache() {
+  int s = second();
+  if (s != tsLastSecond && year() > 2000) {
+    tsLastSecond = s;
     int d = day(); int mo = month(); int y = year();
-    int h = hour(); int mi = minute(); int s = second();
-    ts[0] = '[';
-    ts[1] = '0' + d / 10; ts[2] = '0' + d % 10; ts[3] = '.';
-    ts[4] = '0' + mo / 10; ts[5] = '0' + mo % 10; ts[6] = '.';
-    ts[7] = '0' + y / 1000; ts[8] = '0' + (y / 100) % 10;
-    ts[9] = '0' + (y / 10) % 10; ts[10] = '0' + y % 10;
-    ts[11] = ' ';
-    ts[12] = '0' + h / 10; ts[13] = '0' + h % 10; ts[14] = ':';
-    ts[15] = '0' + mi / 10; ts[16] = '0' + mi % 10; ts[17] = ':';
-    ts[18] = '0' + s / 10; ts[19] = '0' + s % 10;
-    ts[20] = ']'; ts[21] = ' '; ts[22] = 0;
-    Serial.print(ts);
-    logAppend(ts);
+    int h = hour(); int mi = minute();
+    tsCache[0] = '[';
+    tsCache[1] = '0' + d / 10; tsCache[2] = '0' + d % 10; tsCache[3] = '.';
+    tsCache[4] = '0' + mo / 10; tsCache[5] = '0' + mo % 10; tsCache[6] = '.';
+    tsCache[7] = '0' + y / 1000; tsCache[8] = '0' + (y / 100) % 10;
+    tsCache[9] = '0' + (y / 10) % 10; tsCache[10] = '0' + y % 10;
+    tsCache[11] = ' ';
+    tsCache[12] = '0' + h / 10; tsCache[13] = '0' + h % 10; tsCache[14] = ':';
+    tsCache[15] = '0' + mi / 10; tsCache[16] = '0' + mi % 10; tsCache[17] = ':';
+    tsCache[18] = '0' + s / 10; tsCache[19] = '0' + s % 10;
+    tsCache[20] = ']'; tsCache[21] = ' '; tsCache[22] = 0;
+  }
+}
+
+// Zeitstempel ausgeben - liest nur aus dem Cache, keine TimeLib-Aufrufe
+void logTS() {
+  if (tsCache[0] != 0) {
+    Serial.print(tsCache);
+    logAppend(tsCache);
   }
 }
 
@@ -160,7 +169,7 @@ void webHandleMoon();
 void gameface();
 
 #define clockPin 4                //GPIO pin that the LED strip is on
-const char* firmware_version = "1.4";
+const char* firmware_version = "1.5";
 int pixelCount = 120;            //number of pixels in RGB clock
 
 
@@ -521,6 +530,7 @@ void setup() {
     dualOut.println(ntpTime);
     if (ntpTime > 1000000) {
       setTime(ntpTime);
+      updateTimestampCache();
       logTS(); dualOut.print("Zeit gesetzt auf: ");
       dualOut.print(hour());
       dualOut.print(":");
@@ -579,6 +589,7 @@ void loop() {
 
   delay(50);
   if (second() != prevsecond) {
+    updateTimestampCache();
     if (webMode != 0 && second() == 0 && minute() % 10 == 0) { //only record "time of death" if we're in normal running mode.
       EEPROM.begin(512);
       delay(10);
