c024045aefd106aba7357924abcdc38c2a928d39
MikoTec <mikotec82@gmail.com>
2026-05-04 18:37:11 +0000

    v2.3.0.22: Versionsstring-Fix (OTA-Loop behoben), stable+beta auf 2.3.0.22 gesetzt

--- MikoTec-LED-Uhr.ino
+++ MikoTec-LED-Uhr.ino
@@ -257,7 +257,7 @@ int tsLastSecond = -1;
 typedef NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart1Ws2812xMethod> NeoPixelBusType;
 
 #define clockPin 4                //GPIO pin that the LED strip is on
-const char* firmware_version = "2.3.0.21";
+const char* firmware_version = "2.3.0.22";
 int pixelCount = 120;            //number of pixels in RGB clock
 
 
